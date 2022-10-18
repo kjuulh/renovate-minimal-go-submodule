57782e4bd91c574a66ba9a5759bb314d168f6be6
kjuulh <contact@kjuulh.io>
2022-10-18 23:48:41 +0200

    in main.go as well

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	_ "localhost.localdomain/kjuulh/renovate-minimal-go-submodule/submodule-with-user"
+	_ "localhost.localdomain/renovate-minimal-go-submodule"
+	_ "localhost.localdomain/renovate-minimal-go-submodule/submodule"
+)
+
 func main() {
 	println("Hello from root")
 }
